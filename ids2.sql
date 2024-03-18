2ca61769f98c6a26974fef344fc548ff4302fff9
David Cermak <cedada@seznam.cz>
2024-03-18 12:39:43 +0100

    test: Add more test data

--- ids2.sql
+++ ids2.sql
@@ -28,11 +28,11 @@ CREATE TABLE "TKocka" (
 "kocici_cislo" INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
 "jmeno" VARCHAR2(50) NOT NULL,
 "pohlavi" VARCHAR2(10) NOT NULL
-            CHECK("pohlavi" IN ('Male', 'Female', 'Other')) ,
+            CHECK("pohlavi" IN ('Muž', 'Žena', 'Jiné')) ,
 "hmotnost" NUMBER(4,1) NOT NULL ,
 "barva_srsti" VARCHAR2(50) NOT NULL ,
 "opravneni" VARCHAR2(10) NOT NULL
-            CHECK ("opravneni" IN ('User', 'Admin')),
+            CHECK ("opravneni" IN ('Uživatel', 'Admin')),
 "rasa" VARCHAR2(50),
 CONSTRAINT "rasa_kocka_fk"
         FOREIGN KEY ("rasa") REFERENCES "TRasa" ("nazev_rasy")
@@ -44,21 +44,24 @@ CREATE TABLE "THostitel"(
 "jmeno" VARCHAR2(50) NOT NULL,
 "datum_narozeni" DATE NOT NULL,
 "pohlavi" VARCHAR2(10) NOT NULL
-            CHECK ("pohlavi" IN ('Male', 'Female', 'Other')),
+            CHECK ("pohlavi" IN ('Muž', 'Žena', 'Jiné')),
 "ulice" VARCHAR2(50) NOT NULL,
 "mesto" VARCHAR2(50) NOT NULL,
 "psc" SMALLINT NOT NULL
     CHECK(LENGTH("psc") = 5),
 "rc" VARCHAR2(11) UNIQUE NOT NULL
-    CHECK (LENGTH("rc") BETWEEN 9 AND 10 AND MOD(TO_NUMBER("rc"),11) = 0)
+    CHECK (LENGTH("rc") BETWEEN 9 AND 10 AND MOD(TO_NUMBER("rc"),11) = 0 AND REGEXP_LIKE("rc", '^[0-9]+$') AND
+           SUBSTR("rc", 1, 2) BETWEEN '00' AND '99' AND
+           (SUBSTR("rc", 3, 2) BETWEEN '01' AND '12' OR SUBSTR("rc", 3, 2) BETWEEN '51' AND '62') AND
+           SUBSTR("rc", 5, 2) BETWEEN '01' AND '31')
 
 );
 
 CREATE TABLE "TTeritorium" (
 "ID_teritoria" INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
-"nazev" VARCHAR2(50),
+"nazev" VARCHAR2(50) NOT NULL,
 "typ" VARCHAR2(100),
-"kapacita" INT
+"kapacita" INT NOT NULL
 );
 
 CREATE TABLE "TZivot" (
@@ -69,7 +72,7 @@ CREATE TABLE "TZivot" (
 "datum_umrti" DATE DEFAULT NULL,
 "zpusob_umrti" VARCHAR2(100) DEFAULT NULL,
 "id_kocky" INT NOT NULL,
-"id_teritoria" INT,
+"id_teritoria" INT DEFAULT NULL,
 CONSTRAINT "ID_kocka_zivot"
         PRIMARY KEY ("id_kocky", "poradi_zivota"),
 CONSTRAINT "zivot_kockaID_fk"
@@ -96,76 +99,88 @@ CONSTRAINT "predmet_teritoriumID_fk"
 );
 
 CREATE TABLE "TKockaTeritorium"(
-"id_kocky" INT,
-"id_teritoria" INT,
+"id_kocky" INT NOT NULL,
+"id_teritoria" INT NOT NULL,
 CONSTRAINT "ID_kocka_teritorium"
         PRIMARY KEY ("id_kocky", "id_teritoria"),
 CONSTRAINT "kockaID_teritorium_fk"
         FOREIGN KEY ("id_kocky") REFERENCES "TKocka" ("kocici_cislo")
-        ON DELETE SET NULL,
+        ON DELETE CASCADE,
 CONSTRAINT "kocka_teritoriumID_fk"
         FOREIGN KEY ("id_teritoria") REFERENCES "TTeritorium" ("ID_teritoria")
-        ON DELETE SET NULL
+        ON DELETE CASCADE
 );
 
 CREATE TABLE "TKockaHostitel"(
-"id_kocky" INT,
-"id_hostitele" INT,
+"id_kocky" INT NOT NULL,
+"id_hostitele" INT NOT NULL,
 CONSTRAINT "ID_kocka_hostitel"
         PRIMARY KEY ("id_kocky", "id_hostitele"),
 CONSTRAINT "kockaID_hostitel_fk"
         FOREIGN KEY ("id_kocky") REFERENCES "TKocka" ("kocici_cislo")
-        ON DELETE SET NULL,
+        ON DELETE CASCADE,
 CONSTRAINT "kocka_hostitelID_fk"
         FOREIGN KEY ("id_hostitele") REFERENCES "THostitel" ("ID_hostitele")
-        ON DELETE SET NULL
+        ON DELETE CASCADE
 
 );
 
 CREATE TABLE "TRasaHostitel"(
-"id_hostitele" INT,
-"id_rasy" VARCHAR2(100),
+"id_hostitele" INT NOT NULL,
+"id_rasy" VARCHAR2(100) NOT NULL,
 CONSTRAINT "ID_rasa_hostitel"
         PRIMARY KEY ("id_rasy", "id_hostitele"),
 CONSTRAINT "rasaID_hostitel_fk"
         FOREIGN KEY ("id_rasy") REFERENCES "TRasa" ("nazev_rasy")
-        ON DELETE SET NULL,
+        ON DELETE CASCADE,
 CONSTRAINT "rasa_hostitelID_fk"
         FOREIGN KEY ("id_hostitele") REFERENCES "THostitel" ("ID_hostitele")
-        ON DELETE SET NULL
+        ON DELETE CASCADE
 );
 
 CREATE TABLE "TKockaPredmet"(
-"id_kocky" INT,
-"id_predmetu" INT,
+"id_kocky" INT NOT NULL,
+"id_predmetu" INT NOT NULL,
 "od" DATE NOT NULL,
 "do" DATE,
 CONSTRAINT "ID_kocka_predmet_od"
-        PRIMARY KEY ("id_kocky", "id_predmetu", "od"), -- Added "od" into primary key in case of one cat owns single item twice
+        PRIMARY KEY ("id_kocky", "id_predmetu", "od"),
 CONSTRAINT "kockaID_predmet_fk"
         FOREIGN KEY ("id_kocky") REFERENCES "TKocka" ("kocici_cislo")
-        ON DELETE SET NULL,
+        ON DELETE CASCADE,
 CONSTRAINT "kocka_predmetID_fk"
         FOREIGN KEY ("id_predmetu") REFERENCES "TPredmet" ("ID_predmetu")
-        ON DELETE SET NULL
+        ON DELETE CASCADE
 );
 
 
 -- INSERTING TESTING DATA --
 INSERT INTO "TRasa"("nazev_rasy", "barva_oci", "puvod", "prumerna_hmotnost", "prumerny_vek", "charakter")
-VALUES ('Arabská','red','finsko','44.0','12','celkem svina');
+VALUES ('Arabská', 'červená', 'Finsko', '20.0', '12', 'klidná, inteligentní, hravá, společenská');
 INSERT INTO "TRasa"("nazev_rasy", "barva_oci", "puvod", "prumerna_hmotnost", "prumerny_vek", "charakter")
-VALUES ('Bengálská','blue','USA','7.5','10','aktivní, temperamentní kočka, svalnaté a pružné tělo');
+VALUES ('Bengálská','modrá','USA','7.5','10','aktivní, temperamentní, svalnaté a pružné tělo');
+INSERT INTO "TRasa"("nazev_rasy", "barva_oci", "puvod", "prumerna_hmotnost", "prumerny_vek", "charakter")
+VALUES ('Siamská','zelená','Thajsko','5.0','15','inteligentní, aktivní, hlasitá');
+INSERT INTO "TRasa"("nazev_rasy", "barva_oci", "puvod", "prumerna_hmotnost", "prumerny_vek", "charakter")
+VALUES ('Sfinga','žlutá','Egypt','4.0','20','bezsrstá, inteligentní, hravá');
 
 INSERT INTO "TKocka"("jmeno", "pohlavi", "hmotnost", "barva_srsti", "opravneni", "rasa")
-VALUES ('Kocouroš','Male','12.0','orange','User','Arabská');
+VALUES ('Kocouroš','Muž','12.0','oranžová','Uživatel','Arabská');
 INSERT INTO "TKocka"("jmeno", "pohlavi", "hmotnost", "barva_srsti", "opravneni", "rasa")
-VALUES ('Maxík','Male','6.7','black','Admin','Bengálská');
+VALUES ('Maxík','Muž','6.7','černá','Admin','Bengálská');
+INSERT INTO "TKocka"("jmeno", "pohlavi", "hmotnost", "barva_srsti", "opravneni", "rasa")
+VALUES ('Lívaneček', 'Muž', '4.5', 'bílá', 'Uživatel', 'Sfinga');
+INSERT INTO "TKocka"("jmeno", "pohlavi", "hmotnost", "barva_srsti", "opravneni", "rasa")
+VALUES('Micinka', 'Žena', '3.5', 'šedá', 'Uživatel', 'Siamská');
 
 INSERT INTO "THostitel"("jmeno","datum_narozeni","pohlavi","ulice","mesto","psc","rc")
-VALUES ('Alfons Mucha', TO_DATE('1952.03.20', 'yyyy/mm/dd'),'Male','Kolínská 17','Kolín nad Rýnem','71701','0304125635');
+VALUES ('Alfons Mucha', TO_DATE('1952.03.20', 'yyyy/mm/dd'),'Muž','Kolínská 17','Kolín nad Rýnem','71701','5203205634');
 INSERT INTO "THostitel"("jmeno","datum_narozeni","pohlavi","ulice","mesto","psc","rc")
-VALUES ('Kristýna Bačíková', TO_DATE('2004.02.11', 'yyyy/mm/dd'),'Female','507','Petrov','69665','0452115026');
+VALUES ('Kristýna Bačíková', TO_DATE('2004.02.11', 'yyyy/mm/dd'),'Žena','507','Petrov','69665','0452115026');
+INSERT INTO "THostitel"("jmeno","datum_narozeni","pohlavi","ulice","mesto","psc","rc")
+VALUES ('Josefína Nováková', TO_DATE('1978.04.23', 'yyyy/mm/dd'), 'Žena', 'Chaloupky 105', 'Kyjov', '69701', '7804235043');
+INSERT INTO "THostitel"("jmeno","datum_narozeni","pohlavi","ulice","mesto","psc","rc")
+VALUES('David Čermák', TO_DATE('2002.10.07', 'yyyy/mm/dd'), 'Muž', 'Třešňová 1014', 'Strážnice', '69662', '0210074601');
 
 INSERT INTO "TTeritorium"("nazev","typ","kapacita")
 VALUES ('Zahrada','Domov',5);
@@ -173,37 +188,75 @@ INSERT INTO "TTeritorium"("nazev","typ","kapacita")
 VALUES ('Kočičí klub','Klub',20);
 INSERT INTO "TTeritorium"("nazev","typ","kapacita")
 VALUES ('Skládka','Nebezpečí',50);
+INSERT INTO "TTeritorium"("nazev","typ","kapacita")
+VALUES ('Kuchyně','Domov',3);
 
 INSERT INTO "TPredmet"("nazev", "typ", "id_hostitele", "id_teritoria")
-VALUES ('Klubko','hra',NULL,1);
+VALUES ('Klubko', 'hra' ,NULL, 1);
 INSERT INTO "TPredmet"("nazev", "typ", "id_hostitele", "id_teritoria")
-VALUES ('Myš','hra',2,2);
+VALUES ('Myš', 'hra', 2, 2);
+INSERT INTO "TPredmet"("nazev", "typ", "id_hostitele", "id_teritoria")
+VALUES ('Kočičí WC', 'toaleta' ,NULL, 4);
+INSERT INTO "TPredmet"("nazev", "typ", "id_hostitele", "id_teritoria")
+VALUES ('Pilník', 'zbraň', 4, 4);
 
 INSERT INTO "TZivot"("poradi_zivota", "datum_narozeni","misto_narozeni","datum_umrti","zpusob_umrti","id_kocky","id_teritoria")
-VALUES (1, TO_DATE('2019.01.10', 'yyyy/mm/dd'),'Praha',TO_DATE('2021.10.02', 'yyyy/mm/dd'),'umrznutí',1,3);
+VALUES (1, TO_DATE('2019.01.10', 'yyyy/mm/dd'), 'Praha', TO_DATE('2021.10.02', 'yyyy/mm/dd'), 'umrznutí', 1, 3);
 INSERT INTO "TZivot"("poradi_zivota", "datum_narozeni","misto_narozeni","datum_umrti","zpusob_umrti","id_kocky","id_teritoria")
-VALUES (2, TO_DATE('2021.10.02', 'yyyy/mm/dd'),'Praha',NULL,NULL,1,NULL);
+VALUES (2, TO_DATE('2021.10.02', 'yyyy/mm/dd'), 'Praha', NULL, NULL, 1, NULL);
 INSERT INTO "TZivot"("poradi_zivota", "datum_narozeni","misto_narozeni","datum_umrti","zpusob_umrti","id_kocky","id_teritoria")
-VALUES (1, TO_DATE('2017.12.24', 'yyyy/mm/dd'),'Kyjov',NULL,NULL,2,NULL);
+VALUES (1, TO_DATE('2017.12.24', 'yyyy/mm/dd'), 'Kyjov', NULL, NULL, 2, NULL);
+INSERT INTO "TZivot"("poradi_zivota", "datum_narozeni","misto_narozeni","datum_umrti","zpusob_umrti","id_kocky","id_teritoria")
+VALUES (1, TO_DATE('2018.05.05', 'yyyy/mm/dd'), 'Brno', NULL, NULL, 3, NULL);
+INSERT INTO "TZivot"("poradi_zivota", "datum_narozeni","misto_narozeni","datum_umrti","zpusob_umrti","id_kocky","id_teritoria")
+VALUES (1, TO_DATE('2019.01.10', 'yyyy/mm/dd'), 'Veselí nad Moravou', TO_DATE('2021.10.02', 'yyyy/mm/dd'), 'zastřelení', 4, 3);
+INSERT INTO "TZivot"("poradi_zivota", "datum_narozeni","misto_narozeni","datum_umrti","zpusob_umrti","id_kocky","id_teritoria")
+VALUES (2, TO_DATE('2021.10.02', 'yyyy/mm/dd'), 'Veselí nad Moravou', TO_DATE('2023.05.05', 'yyyy/mm/dd'), 'pád z výšky', 4, 1);
+INSERT INTO "TZivot"("poradi_zivota", "datum_narozeni","misto_narozeni","datum_umrti","zpusob_umrti","id_kocky","id_teritoria")
+VALUES (3, TO_DATE('2023.05.05', 'yyyy/mm/dd'), 'Uherský Ostroh', NULL, NULL, 4, NULL);
+
 INSERT INTO "TKockaTeritorium"("id_kocky","id_teritoria")
 VALUES (1,1);
-
 INSERT INTO "TKockaTeritorium"("id_kocky","id_teritoria")
 VALUES (1,3);
 INSERT INTO "TKockaTeritorium"("id_kocky","id_teritoria")
 VALUES (2,2);
-INSERT INTO "TKockaHostitel"("id_kocky","id_hostitele")
-VALUES (1,1);
+INSERT INTO "TKockaTeritorium"("id_kocky","id_teritoria")
+VALUES (3,4);
+INSERT INTO "TKockaTeritorium"("id_kocky","id_teritoria")
+VALUES (4,2);
 
 INSERT INTO "TKockaHostitel"("id_kocky","id_hostitele")
+VALUES (1,1);
+INSERT INTO "TKockaHostitel"("id_kocky","id_hostitele")
 VALUES (2,2);
+INSERT INTO "TKockaHostitel"("id_kocky","id_hostitele")
+VALUES (3,3);
+INSERT INTO "TKockaHostitel"("id_kocky","id_hostitele")
+VALUES (4,4);
+INSERT INTO "TKockaHostitel"("id_kocky","id_hostitele")
+VALUES (4,3);
+
 INSERT INTO "TRasaHostitel"("id_rasy","id_hostitele")
 VALUES ('Arabská',1);
+INSERT INTO "TRasaHostitel"("id_rasy","id_hostitele")
+VALUES ('Bengálská',3);
+INSERT INTO "TRasaHostitel"("id_rasy","id_hostitele")
+VALUES ('Sfinga',3);
+INSERT INTO "TRasaHostitel"("id_rasy","id_hostitele")
+VALUES ('Siamská',4);
+INSERT INTO "TRasaHostitel"("id_rasy","id_hostitele")
+VALUES ('Sfinga',2);
 
 INSERT INTO "TKockaPredmet"("id_kocky","id_predmetu","od","do")
 VALUES (1,1,TO_DATE('2021.10.05', 'yyyy/mm/dd'),NULL);
-
 INSERT INTO "TKockaPredmet"("id_kocky","id_predmetu","od","do")
 VALUES (2,2,TO_DATE('2023.05.05', 'yyyy/mm/dd'), TO_DATE('2024.01.01', 'yyyy/mm/dd'));
+INSERT INTO "TKockaPredmet"("id_kocky","id_predmetu","od","do")
+VALUES (3,3,TO_DATE('2022.11.12', 'yyyy/mm/dd'),NULL);
+INSERT INTO "TKockaPredmet"("id_kocky","id_predmetu","od","do")
+VALUES (4,4,TO_DATE('2020.12.24', 'yyyy/mm/dd'),NULL);
+INSERT INTO "TKockaPredmet"("id_kocky","id_predmetu","od","do")
+VALUES (4,2,TO_DATE('2024.01.03', 'yyyy/mm/dd'),NULL);
 
 COMMIT;
